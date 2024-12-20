02903f4d5e4c795d65565d3290fd084d367954f8
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-20 16:58:51 -0600

    Complete js01

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function( string, num ) {
+    
+    if (num<0) return 'ERROR';
 
+    let output = '';
+
+    while ( num>0 ) {
+
+        output += string;
+        num--;
+
+    }
+
+    return output;
 };
 
 // Do not edit below this line
