2868bac630306805d01c1b4e5aefbf3386488bc0
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-20 17:43:50 -0600

    Complete 04 js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function( array, ...valuesToBeRemoved) {
+
+    // this function returns false if array entry is included to be removed
+    let removeValuesFilter = function(valueChecked) {
+        return (valuesToBeRemoved.includes(valueChecked)) ? false : true;
+    }
+
+    return array.filter(removeValuesFilter);
+
 };
 
 // Do not edit below this line
