d05bbad1f3fa0a37982941c24aa4b973f6e418b8
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-20 20:32:54 -0600

    Complete js 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function( num1, num2) {
+    //error states
+    if (typeof num1 != 'number' || typeof num2 != 'number') return 'ERROR';
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (num1%1 != 0 || num2%1 != 0) return 'ERROR';
+    
+    if (num1 > num2) {
+        const tempNum = num1;
+        num1 = num2;
+        num2 = tempNum;
+    }
+    
+    let sum = 0;
+    
+    for (let i=num1; i<num2+1; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
