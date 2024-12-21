9021f3a2c6e63cbd8c096b042bf0aef291de7bcd
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-20 20:39:33 -0600

    Complete 06 js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function( num ) {
+    if ( typeof num != 'number' || num<0 ) return 'ERROR';
 
+    if ( num%4 == 0 && ( num%100 != 0 || num%400 == 0)) return true;
+    else return false;
 };
 
 // Do not edit below this line
